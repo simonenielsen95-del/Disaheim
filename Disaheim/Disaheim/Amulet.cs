1bf1461bf8b700c97e8e34bb52ba88d62d97bc3f
Simone Nielsen <simonenielsen95@gmail.com>
2025-10-08 11:33:12 +0200

    bog, amulet..

--- Disaheim/Disaheim/Amulet.cs
+++ Disaheim/Disaheim/Amulet.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Disaheim
 {
-    internal class Amulet
+    public class Amulet
     {
-        enum Level
+
+        public string ItemId;        
+        public string Design;
+        public Level Quality;
+       
+
+        public Amulet(string itemId)
         {
-            low,
-            medium,
-            high
+            ItemId = itemId;
+            Design = "";
+            Quality = Level.medium;
         }
-        public void 
+
+        public Amulet(string itemId, Level quality)
+        {
+            ItemId = itemId;
+            Design = "";
+            Quality = quality;           
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Design = design;
+            Quality = quality;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
     }
 }
---
+++ Disaheim/Disaheim/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        public string ItemId;
+        public string Title;
+        public double Price;
+ 
+
+        public Book(string itemId)
+        { 
+            ItemId = itemId;
+            Title = "";
+            Price = 0;        
+        }
+        public Book(string itemId, string title)
+        {
+            ItemId = itemId;
+            Title = title;
+            Price = 0;
+        }
+
+        public Book(string itemId, string title, double price)
+        {
+            ItemId = itemId;
+            Title = title;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Title: {Title}, Price: {Price}";
+        }
+        
+    }   
+
+}
