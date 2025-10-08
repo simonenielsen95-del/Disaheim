8dea0e9223572b8133072c42695596c7cd1a6c0a
Simone Nielsen <simonenielsen95@gmail.com>
2025-10-08 09:56:46 +0200

    hello

---
+++ Disaheim/Disaheim/Amulet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Amulet
+    {
+        enum Level
+        {
+            low,
+            medium,
+            high
+        }
+        public void 
+    }
+}
