84dc0af7f0451c2dd3ecfb3a7896581c562c48cd
Simone Nielsen <simonenielsen95@gmail.com>
2025-10-09 12:05:38 +0200

    tilføjet controller og unit test 2 og 3

---
+++ Disaheim/Disaheim/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        public List<Book> Books;
+        public List<Amulet> Amulets;
+
+        public Controller()
+        {
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+        public void AddToList(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void AddToList(Amulet amulet)
+        {
+            Amulets.Add(amulet);
+        }
+    }
+}
---
+++ Disaheim/DisaheimTest/UnitTest2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Disaheim;
+namespace DisaheimTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        Book b1, b2, b3;
+        Amulet a1, a2, a3;
+        Utility utility;
+
+        [TestInitialize]
+        public void Init()
+        {
+            // Arrange
+
+            b1 = new Book("1");
+            b2 = new Book("2", "Falling in Love with Yourself");
+            b3 = new Book("3", "Spirits in the Night", 123.55);
+
+            a1 = new Amulet("11");
+            a2 = new Amulet("12", Level.High);
+            a3 = new Amulet("13", Level.Low, "Capricorn");
+
+            utility = new Utility();
+        }
+
+        [TestMethod]
+        public void TestGetValueForBook1()
+        {
+            // Assert
+            Assert.AreEqual(0.0, utility.GetValueOfBook(b1));
+        }
+        [TestMethod]
+        public void TestGetValueForBook2()
+        {
+            // Assert
+            Assert.AreEqual(0.0, utility.GetValueOfBook(b2));
+        }
+        [TestMethod]
+        public void TestGetValueForBook3()
+        {
+            // Assert
+            Assert.AreEqual(123.55, utility.GetValueOfBook(b3));
+        }
+
+        [TestMethod]
+        public void TestGetValueForAmulet1()
+        {
+            // Assert
+            Assert.AreEqual(20.0, utility.GetValueOfAmulet(a1));
+        }
+        [TestMethod]
+        public void TestGetValueForAmulet2()
+        {
+            // Assert
+            Assert.AreEqual(27.5, utility.GetValueOfAmulet(a2));
+        }
+        [TestMethod]
+        public void TestGetValueForAmulet3()
+        {
+            // Assert
+            Assert.AreEqual(12.5, utility.GetValueOfAmulet(a3));
+        }
+    }
+
+}
